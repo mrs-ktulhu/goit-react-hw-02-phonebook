3499ff8cb7d79f523290405428b923e5a42dda42
Mamba <113925095+mrs-ktulhu@users.noreply.github.com>
2023-09-18 02:46:12 +0300

    fix  delete button

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -3,9 +3,10 @@ import { ContactListItem } from './ContactListItem';
 import { ContactsList } from './ConatctList.styled';
 
 const ContactList = ({ contacts, onDeleteContact }) => (
+  
   <ContactsList>
     {contacts.map(({ id, text, number }) => (
-      <ContactListItem key={id} text={text} number={number} onDeleteContact={onDeleteContact} />
+      <ContactListItem key={id} id={id} text={text} number={number} onDeleteContact={onDeleteContact} />
     ))}
   </ContactsList>
 );
