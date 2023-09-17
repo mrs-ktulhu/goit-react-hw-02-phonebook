05e4888597e54df99998df80a02e1e044aa22b4b
Mamba <113925095+mrs-ktulhu@users.noreply.github.com>
2023-09-18 02:26:51 +0300

    add conditional clear of input "name"

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,7 +4,6 @@ import ContactForm from './ContactForm/ContactForm';
 import ContactList from './ContactList/ContactList';
 import Filter from './Filter/Filter';
 
-
 export default class App extends Component {
   state = {
     contacts: [
@@ -21,11 +20,9 @@ export default class App extends Component {
   };
 
   handleAddContact = (text, number) => {
-    this.state.contacts.some(contact => contact.text === text)
-      ? alert(`${text} is already in contacts.`)
-      : this.setState(prevState => ({
-          contacts: [{ id: nanoid(), text, number }, ...prevState.contacts],
-        }));
+    this.setState(prevState => ({
+      contacts: [{ id: nanoid(), text, number }, ...prevState.contacts],
+    }));
   };
 
   deleteContact = contactId => {
@@ -42,9 +39,9 @@ export default class App extends Component {
     );
 
     return (
-      <div className='Container'>
+      <div className="Container">
         <h1>Phonebook</h1>
-        <ContactForm onSubmit={this.handleAddContact} />
+        <ContactForm contacts={this.state.contacts} onSubmit={this.handleAddContact} />
 
         <h2>Contacts</h2>
         <Filter filter={this.state.filter} changeFilter={this.changeFilter} />
--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -12,12 +12,21 @@ export default class ContactForm extends Component {
     this.setState({ [e.currentTarget.name]: e.currentTarget.value });
   };
 
-  handleSubmit = e => {
-    e.preventDefault();
 
-    this.props.onSubmit(this.state.name, this.state.number);
-    this.setState({ name: '', number: '' });
-  };
+handleSubmit = e => {
+  e.preventDefault();
+
+  const { name, number } = this.state;
+
+  if (this.props.contacts.some(contact => contact.text === name&&contact.number===number)) {
+    alert(`${name} is already in contacts.`);
+    this.setState({ name: '' }); 
+  } else {
+    this.props.onSubmit(name, number);
+    this.setState({ name: '', number: '' }); 
+  }
+};
+
 
   render() {
     return (
